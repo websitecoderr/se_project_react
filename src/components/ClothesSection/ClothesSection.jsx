24ed48d9e6570f5509b0d903d9536589a3c175d9
Alfa <opportunities91@gmail.com>
2025-06-16 12:44:36 -0700

    project update

--- src/components/ClothesSection/ClothesSection.jsx
+++ src/components/ClothesSection/ClothesSection.jsx
@@ -1,5 +1,7 @@
+import React from "react";
 import ItemCard from "../ItemCard/ItemCard";
 import "./ClothesSection.css";
+import { useCurrentUser } from "../../Context/CurrentUserContext";
 
 function ClothesSection({
   onCardClick,
@@ -8,6 +10,8 @@ function ClothesSection({
   handleAddClick,
   onCardLike,
 }) {
+  const { currentUser } = useCurrentUser();
+
   return (
     <div className="clothes-section">
       <div className="clothes-section__headers" style={{ display: "flex" }}>
@@ -15,22 +19,24 @@ function ClothesSection({
         <button
           onClick={handleAddClick}
           type="button"
-          className="clothes-section__add-button "
+          className="clothes-section__add-button"
         >
           + Add New
         </button>
       </div>
       <div className="clothes-section__cards">
         {filteredClothingItems.length > 0 ? (
-          filteredClothingItems.map((card) => (
-            <ItemCard
-              key={card._id}
-              item={card}
-              onCardClick={onCardClick}
-              onCardDelete={onCardDelete}
-              onCardLike={onCardLike}
-            />
-          ))
+          filteredClothingItems
+            .filter((item) => item.owner === currentUser?._id)
+            .map((card) => (
+              <ItemCard
+                key={card._id}
+                item={card}
+                onCardClick={onCardClick}
+                onCardDelete={onCardDelete}
+                onCardLike={onCardLike}
+              />
+            ))
         ) : (
           <p className="clothes-section__empty">
             You don&apos;t have any items yet.
